3507c6f3b03beff72e4ff46376e08a32f6d28add
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-12-02 18:35:03 -0300

    11 - Three Part Numeric Format

---
+++ Tips/Tips.Core/11 - Three Part Numeric Format/ThreePartNumericFormatTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tips.Core
+{
+    [TestClass]
+    public class ThreePartNumericFormatTest
+    {
+        [TestMethod]
+        public void Example()
+        {
+            const double positiveNumber = 99.99;
+            const double negativeNumber = -23.55;
+            const double zeroNumber = 0;
+
+
+            const string threePartFormat = "(Positivo)#.##;(Negativo)#.##;(Número Cero)";
+
+
+            var positiveOutput = positiveNumber.ToString(threePartFormat);
+            var negativeOutput = negativeNumber.ToString(threePartFormat);
+            var zeroOutput = zeroNumber.ToString(threePartFormat);
+
+
+            Debug.IndentLevel = 10;
+            Debug.WriteLine(positiveNumber);
+            Debug.WriteLine(negativeOutput);
+            Debug.WriteLine(zeroOutput);
+        }
+    }
+}
