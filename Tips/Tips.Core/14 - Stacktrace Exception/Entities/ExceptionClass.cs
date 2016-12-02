be1cd794041e58e0a11d0656c633ca92cf61d11f
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-12-02 19:03:20 -0300

    14 - Stacktrace Exception

---
+++ Tips/Tips.Core/14 - Stacktrace Exception/Entities/ExceptionClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Tips.Core.Entities
+{
+    class ExceptionClass
+    {
+        public void NoStacktrace()
+        {
+            try
+            {
+                MethodA();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+        public void WithStacktrace()
+        {
+            try
+            {
+                MethodA();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+
+        private void MethodA()
+        {
+            MethodB();
+        }
+
+        private void MethodB()
+        {
+            MethodC();
+        }
+
+        private void MethodC()
+        {
+            throw new ApplicationException("First Exception");
+        }
+    }
+}
---
+++ Tips/Tips.Core/14 - Stacktrace Exception/StacktraceExceptionTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tips.Core.Entities;
+
+namespace Tips.Core
+{
+    [TestClass]
+    public class StacktraceExceptionTest
+    {
+        [TestMethod]
+        public void NoStacktraceTest()
+        {
+            var w = new ExceptionClass();
+
+            w.NoStacktrace();
+        }
+
+
+        [TestMethod]
+        public void WithStacktraceTest()
+        {
+            var w = new ExceptionClass();
+
+            w.WithStacktrace();
+        }
+    }
+}
