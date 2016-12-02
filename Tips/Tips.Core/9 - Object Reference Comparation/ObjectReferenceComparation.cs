cec2088cf1f91708bf9b8a8f14811c967f91fd77
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-12-02 18:21:23 -0300

    9 - Object Reference Comparation

---
+++ Tips/Tips.Core/9 - Object Reference Comparation/ObjectReferenceComparation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tips.Core
+{
+    [TestClass]
+    public class ObjectReferenceComparationTest
+    {
+        [TestMethod]
+        public void CompareReferenceTest()
+        {
+            var a = new TestClass { Name = "First Name" };
+            var b = new TestClass { Name = "First Name" };
+
+            var isEqual = a == b;
+
+            var isSameReference = object.ReferenceEquals(a, b);
+
+            b = a;
+
+            isSameReference = object.ReferenceEquals(a, b);
+        }
+
+
+
+
+
+
+        [TestMethod]
+        public void ExampleWithStringLiterals()
+        {
+            var s1 = "Hello";
+            var s2 = "Hello";
+
+            var isSameReference = object.ReferenceEquals(s1, s2);
+        }
+    }
+
+
+
+
+    class TestClass
+    {
+        public string Name { get; set; }
+    }
+}
