9b396d8de317e7d48a59c4f539103a6ee6fdb690
GermanKuber <GermanKuber@DESKTOP-AL36CKD>
2016-12-14 16:44:32 -0300

    22 - Struct Equals Performance

---
+++ Tips/Tips/22 - Struct Equals Performance/Entities/NoRefNoOverride.cs
@@ -0,0 +1,8 @@
+﻿namespace Tips.Entities
+{
+    internal struct NoRefNoOverride
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ Tips/Tips/22 - Struct Equals Performance/Entities/WithRefNoOverride.cs
@@ -0,0 +1,10 @@
+﻿namespace Tips.Entities
+{
+    internal struct WithRefNoOverride
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Description { get; set; }
+    }
+}
+
